9a8c0de10d63c09a90446c12ab3da4475979d9e5
frost_x <zakgleb@gmail.com>
2024-02-09 01:02:18 +0300

    Install project

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ import psutil
 #блокировка, pet-cpu
 #print(psutil.cpu_percent(interval=1, percpu=True))
 
-print(psutil.cpu_times_percent(interval=None, percpu=False))
+#print(psutil.cpu_times_percent(interval=None, percpu=False))
+
+print(psutil.cpu_count())
+print(psutil.cpu_count(logical=True))
+print(psutil.cpu_count(logical=False))
