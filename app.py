5a351240ba005828cfcbc0c2c270ed62e8175b46
frost_x <zakgleb@gmail.com>
2024-02-09 00:26:56 +0300

    Install project

---
+++ app.py
@@ -0,0 +1,8 @@
+import psutil
+print(psutil.cpu_times())
+#blocking
+print(psutil.cpu_percent(interval=1))
+# non-blocking (процент с момента последнего звонка)
+print(psutil.cpu_percent(interval=None))
+#блокировка, pet-cpu
+print(psutil.cpu_percent(interval=1, percpu=True))
